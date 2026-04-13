ff5890f446c639a2bee3b4610e5ed14a9b647339
appuser <appuser@ai-guardian-remediation-6fd95659bd-lmbzv>
2026-04-13 13:20:59 +0000

    fix: cookie-issecure-false-58

--- src/main/java/org/owasp/webgoat/lessons/spoofcookie/SpoofCookieAssignment.java
+++ src/main/java/org/owasp/webgoat/lessons/spoofcookie/SpoofCookieAssignment.java
@@ -57,6 +57,7 @@ public class SpoofCookieAssignment implements AssignmentEndpoint {
   public void cleanup(HttpServletResponse response) {
     Cookie cookie = new Cookie(COOKIE_NAME, "");
     cookie.setMaxAge(0);
+    cookie.setSecure(true);
     response.addCookie(cookie);
   }
 
